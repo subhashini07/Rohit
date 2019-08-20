56869835d9c0dfa59afe973548e56ccff111fabe
sirisha2002 <sirishapoorna@gmail.com>
2019-08-20 12:44:17 +0530

    java

--- src/org/cts/JavaExecutor.java
+++ src/org/cts/JavaExecutor.java
@@ -7,6 +7,8 @@ public static void main(String[] args) {
 	System.out.println("Jay");
     System.out.println("archana");
     System.out.println("siri");
+   System.out.println("123");
+   System.out.println("wer");
 
 }
 }
