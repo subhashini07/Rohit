3b102b1bbe977335483f6ae1f21ea310ba5aa5d8
Karthika0512 <Karthikarajasekaran1993@gmail.com>
2019-08-20 12:12:28 +0530

    mess

--- src/org/cts/JavaExecutor.java
+++ src/org/cts/JavaExecutor.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	System.out.println("HI");
 
 	System.out.println("Karthika");
-	System.out.println("greens");
+	System.out.println("greens1");
 	System.out.println("Jay");
 	
 
